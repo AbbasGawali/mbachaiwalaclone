f1a54f82cf9e06be459789fd51ce565edea672c0
AbbasGawali <103979654+AbbasGawali@users.noreply.github.com>
2023-07-11 11:30:28 +0530

    minor refactor

--- src/App.js
+++ src/App.js
@@ -5,177 +5,180 @@ import "./styles/footer.scss";
 import React, { useEffect } from "react";
 import Intro from "./components/Intro";
 import Section from "./components/Section";
-import freshTopicImg from "./assets/academy.png"
-import data from "./data/data.json"
-import freshTopic2Img from "./assets/story.png"
+import freshTopicImg from "./assets/academy.png";
+import data from "./data/data.json";
+import freshTopic2Img from "./assets/story.png";
 
 import "./styles/mediaquery.scss";
-import tedTalksImg from "./assets/tedTalk.gif"
-import franchiseImg from "./assets/franchise.gif"
-import mapImg from "./assets/locations.png"
-import coursesImg from "./assets/image2.png"
-import albumImg from "./assets/mba-cares.gif"
-import baratImg from "./assets/image1.png"
-import chaiWalaImg from "./assets/image3.png"
+import tedTalksImg from "./assets/tedTalk.gif";
+import franchiseImg from "./assets/franchise.gif";
+import mapImg from "./assets/locations.png";
+import coursesImg from "./assets/image2.png";
+import albumImg from "./assets/mba-cares.gif";
+import baratImg from "./assets/image1.png";
+import chaiWalaImg from "./assets/image3.png";
 import Footer from "./components/Footer";
 import Misc from "./components/Misc";
 
-const yellow = "#fff100", pink = "#ed1e79", white = "#fff", brown = "#6d3d0f";
+const yellow = "#fff100",
+  pink = "#ed1e79",
+  white = "#fff",
+  brown = "#6d3d0f";
 
 const App = () => {
+  const {
+    freshTopic,
+    freshTopic2,
+    tedTalks,
+    franchise,
+    map,
+    courses,
+    album,
+    barat,
+    chaiwala,
+  } = data;
+  const dotCursor = (e) => {
+    const cursor = document.querySelector(".cursor");
 
-    const { freshTopic, freshTopic2, tedTalks, franchise, map, courses, album, barat, chaiwala } = data;
-    const dotCursor = (e) => {
-        const cursor = document.querySelector(".cursor");
+    cursor.style.top = `${e.pageY - 14}px`;
+    cursor.style.left = `${e.pageX - 14}px`;
 
-        cursor.style.top = `${e.pageY - 14}px`
-        cursor.style.left = `${e.pageX - 14}px`
-
-        const element = e.target;
-        if (element.getAttribute("data-cursorpointer")) {
-            cursor.classList.add("cursorHover")
-        } else if (element.getAttribute("data-cursorpointermini")) {
-            cursor.classList.add("cursorHoverMini")
-
-        }
-        else {
-            cursor.classList.remove("cursorHover")
-            cursor.classList.remove("cursorHoverMini")
-
-        }
+    const element = e.target;
+    if (element.getAttribute("data-cursorpointer")) {
+      cursor.classList.add("cursorHover");
+    } else if (element.getAttribute("data-cursorpointermini")) {
+      cursor.classList.add("cursorHoverMini");
+    } else {
+      cursor.classList.remove("cursorHover");
+      cursor.classList.remove("cursorHoverMini");
     }
+  };
 
-    useEffect(() => {
+  useEffect(() => {
+    window.addEventListener("mousemove", dotCursor);
+    return () => {
+      window.addEventListener("mousemove", dotCursor);
+    };
+  }, []);
 
-        window.addEventListener("mousemove", dotCursor)
-        return () => {
-            window.addEventListener("mousemove", dotCursor)
+  return (
+    <>
+      <Intro />
 
-        }
-    }, [])
+      <Section
+        h3={freshTopic.heading}
+        text={freshTopic.text}
+        btnText={freshTopic.btn}
+        imgSrc={freshTopicImg}
+        backgroundColor={pink}
+        headingColor={yellow}
+        textColor={yellow}
+        btnBgColor={yellow}
+        btnColor={pink}
+      />
 
-    return (
-        <>
-            <Intro />
+      <Section
+        h3={freshTopic2.heading}
+        text={freshTopic2.text}
+        btnText={freshTopic2.btn}
+        imgSrc={freshTopic2Img}
+        backgroundColor={pink}
+        headingColor={yellow}
+        textColor={yellow}
+        btnBgColor={yellow}
+        btnColor={pink}
+      />
 
+      <Section
+        h3={tedTalks.heading}
+        text={tedTalks.text}
+        btnText={tedTalks.btn}
+        imgSrc={tedTalksImg}
+        backgroundColor={yellow}
+        headingColor={pink}
+        textColor={pink}
+        btnBgColor={pink}
+        btnColor={yellow}
+      />
 
-            <Section h3={freshTopic.heading}
-                text={freshTopic.text}
-                btnText={freshTopic.btn}
-                imgSrc={freshTopicImg}
-                backgroundColor={pink}
-                headingColor={yellow}
-                textColor={yellow}
-                btnBgColor={yellow}
-                btnColor={pink}
-            />
+      <Section
+        h3={franchise.heading}
+        text={franchise.text}
+        btnText={franchise.btn}
+        imgSrc={franchiseImg}
+        backgroundColor={white}
+        headingColor={pink}
+        textColor={brown}
+        btnBgColor={brown}
+        btnColor={yellow}
+      />
 
+      <Section
+        h3={map.heading}
+        text={map.text}
+        btnText={map.btn}
+        imgSrc={mapImg}
+        backgroundColor={pink}
+        headingColor={yellow}
+        textColor={yellow}
+        // btnBgColor={brown}
+        // btnColor={yellow}
+        hasBtn={false}
+      />
 
+      <Section
+        h3={courses.heading}
+        text={courses.text}
+        btnText={courses.btn}
+        imgSize={"30%"}
+        imgSrc={coursesImg}
+        backgroundColor={yellow}
+        headingColor={pink}
+        textColor={pink}
+        btnBgColor={pink}
+        btnColor={yellow}
+      />
 
+      <Section
+        h3={album.heading}
+        text={album.text}
+        btnText={album.btn}
+        // imgSize={"30%"}
+        imgSrc={albumImg}
+        backgroundColor={white}
+        headingColor={pink}
+        textColor={brown}
+        btnBgColor={brown}
+        btnColor={yellow}
+      />
 
-            <Section h3={freshTopic2.heading}
-                text={freshTopic2.text}
-                btnText={freshTopic2.btn}
-                imgSrc={freshTopic2Img}
-                backgroundColor={pink}
-                headingColor={yellow}
-                textColor={yellow}
-                btnBgColor={yellow}
-                btnColor={pink}
-            />
+      <Section
+        h3={barat.heading}
+        text={barat.text}
+        btnText={barat.btn}
+        imgSrc={baratImg}
+        backgroundColor={pink}
+        headingColor={yellow}
+        textColor={yellow}
+        btnBgColor={yellow}
+        btnColor={pink}
+      />
 
-
-            <Section h3={tedTalks.heading}
-                text={tedTalks.text}
-                btnText={tedTalks.btn}
-                imgSrc={tedTalksImg}
-                backgroundColor={yellow}
-                headingColor={pink}
-                textColor={pink}
-                btnBgColor={pink}
-                btnColor={yellow}
-            />
-
-            <Section h3={franchise.heading}
-                text={franchise.text}
-                btnText={franchise.btn}
-                imgSrc={franchiseImg}
-                backgroundColor={white}
-                headingColor={pink}
-                textColor={brown}
-                btnBgColor={brown}
-                btnColor={yellow}
-            />
-
-
-            <Section h3={map.heading}
-                text={map.text}
-                btnText={map.btn}
-                imgSrc={mapImg}
-                backgroundColor={pink}
-                headingColor={yellow}
-                textColor={yellow}
-                // btnBgColor={brown}
-                // btnColor={yellow}
-                hasBtn={false}
-            />
-
-            <Section h3={courses.heading}
-                text={courses.text}
-                btnText={courses.btn}
-                imgSize={"30%"}
-                imgSrc={coursesImg}
-                backgroundColor={yellow}
-                headingColor={pink}
-                textColor={pink}
-                btnBgColor={pink}
-                btnColor={yellow}
-
-            />
-
-            <Section h3={album.heading}
-                text={album.text}
-                btnText={album.btn}
-                // imgSize={"30%"}
-                imgSrc={albumImg}
-                backgroundColor={white}
-                headingColor={pink}
-                textColor={brown}
-                btnBgColor={brown}
-                btnColor={yellow}
-
-            />
-
-            <Section h3={barat.heading}
-                text={barat.text}
-                btnText={barat.btn}
-                imgSrc={baratImg}
-                backgroundColor={pink}
-                headingColor={yellow}
-                textColor={yellow}
-                btnBgColor={yellow}
-                btnColor={pink}
-
-            />
-
-            <Section h3={chaiwala.heading}
-                text={chaiwala.text}
-                btnText={chaiwala.btn}
-                imgSrc={chaiWalaImg}
-                backgroundColor={white}
-                headingColor={pink}
-                textColor={brown}
-                btnBgColor={brown}
-                btnColor={yellow}
-
-            />
-            <Footer />
-            <Misc />
-
-
-        </>
-    )
-}
-
+      <Section
+        h3={chaiwala.heading}
+        text={chaiwala.text}
+        btnText={chaiwala.btn}
+        imgSrc={chaiWalaImg}
+        backgroundColor={white}
+        headingColor={pink}
+        textColor={brown}
+        btnBgColor={brown}
+        btnColor={yellow}
+      />
+      <Footer />
+      <Misc />
+    </>
+  );
+};
 
 export default App;
